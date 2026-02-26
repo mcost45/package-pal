2d44260d19256d7dd1bf2ea5cee7f7a6716d2f85
Matthew Costello <mcost45@hotmail.com>
2026-02-26 10:01:53 +0100

    Fix: handle file deletions in watcher

--- packages/core/src/lib/watch/functions/filter-files-modified-since.ts
+++ packages/core/src/lib/watch/functions/filter-files-modified-since.ts
@@ -1,6 +1,9 @@
 export const filterFilesModifiedSince = (paths: string[], sinceMs: number) => {
 	return paths.filter((path) => {
 		const changedFile = Bun.file(path);
-		return changedFile.lastModified >= sinceMs;
+		const isDeleted = changedFile.lastModified === 0;
+		const isModifiedSince = changedFile.lastModified >= sinceMs;
+
+		return isDeleted || isModifiedSince;
 	});
 };
